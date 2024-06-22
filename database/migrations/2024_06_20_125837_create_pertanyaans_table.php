63687fb0be92cd72ab86a6bbc826c148fb4f615a
Gofar21 <abdulgofarm@gmail.com>
2024-06-22 22:26:04 +0700

    migration pertanyaan

---
+++ database/migrations/2024_06_20_125837_create_pertanyaans_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePertanyaansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pertanyaans', function (Blueprint $table) {
+            $table->id();
+            $table->text('text_payload');
+            $table->string('label_type');
+            $table->text('text_input');
+            $table->integer('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pertanyaans');
+    }
+}
